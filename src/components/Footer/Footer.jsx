122025489b2e1ff76978f775258d3270114a2030
amankhare12 <amankhare324@gmail.com>
2025-10-18 16:37:33 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright @ {new Date().getFullYear()} - All Right Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
